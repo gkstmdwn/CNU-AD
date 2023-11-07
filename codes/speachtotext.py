4430f8a231cda3d0b773070b0e39a76b7b857b0e
SeungJoo Han <sugarcanes0118@gmail.com>
2023-11-07 15:57:07 +0900

    FInal

--- codes/speachtotext.py
+++ codes/speachtotext.py
@@ -4,4 +4,7 @@ def s2t(audiofile:str) -> str:
     r = speech_recognition.Recognizer()
     with speech_recognition.AudioFile(audiofile) as source:
         audio = r.record(source)
-    return r.recognize_google(audio, language='ko-KR')
+    return r.recognize_google(audio, language='en')
+
+if __name__ == "__main__":
+    print(s2t("tts.wav"))
--- codes/texttospeech.py
+++ codes/texttospeech.py
@@ -4,3 +4,6 @@ import os
 def tts(text:str) -> gTTS:
     audio = gTTS(text=text, lang='en', slow=False)
     return audio
+
+if __name__ == "__main__":
+    tts("Hello sir, my name is SeungJoo").save('tts.wav')
